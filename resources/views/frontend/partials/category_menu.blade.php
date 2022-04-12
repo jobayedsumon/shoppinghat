408d03177304275a2331cc5d325a5fe18b960152
JobayedSumon <jobayed.ahamed@apsissolutions.com>
2022-04-13 02:58:45 +0600

    Category link fixed

--- resources/views/frontend/partials/category_menu.blade.php
+++ resources/views/frontend/partials/category_menu.blade.php
@@ -10,7 +10,7 @@
 
                     @foreach (\App\Models\Category::where('level', 0)->orderBy('order_level', 'desc')->get()->take(11) as $key => $category)
                         <li>
-                            <a href="javascript:void(0);" class="menu-link mmega-menu-link" aria-haspopup="true">{{ $category->getTranslation('name') }}</a>
+                            <a href="{{ route('products.category', $category->slug) }}" class="menu-link mmega-menu-link" aria-haspopup="true">{{ $category->getTranslation('name') }}</a>
 
 
                         @if(count(\App\Utility\CategoryUtility::get_immediate_children_ids($category->id))>0)
@@ -19,7 +19,7 @@
 
                                 @foreach (\App\Utility\CategoryUtility::get_immediate_children_ids($category->id) as $key => $first_level_id)
                                 <li>
-                                    <a href="javascript:void(0);" class="menu-link menu-list-link"
+                                    <a href="{{ route('products.category', \App\Models\Category::find($first_level_id)->slug) }}" class="menu-link menu-list-link"
                                         aria-haspopup="true"> {{ \App\Models\Category::find($first_level_id)->getTranslation('name') }}</a>
                                        @if( count(\App\Utility\CategoryUtility::get_immediate_children_ids($first_level_id))>0)
                                     <ul class="menu menu-list">
